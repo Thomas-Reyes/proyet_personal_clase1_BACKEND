e5f3e013adc54365325d0b62499536763a131201
thomas reyes <thomas.alonsoreyesmail.com>
2024-02-07 14:42:19 -0300

    instalacion plugins transformer/cloudinari/documentation

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,31 @@
-export default () => ({});
+export default  ({env}) => ({
+
+'transformer':{
+    enable: true,
+    config:{
+        prefix: '/api/',
+        responseTransforms: {
+            removeAttributesKey: true,
+            removeDataKey: true,
+        }
+      }
+    },
+    upload: {
+        config: {
+          provider: 'cloudinary',
+          providerOptions: {
+            cloud_name: env('CLOUDINARY_NAME'),
+            api_key: env('CLOUDINARY_KEY'),
+            api_secret: env('CLOUDINARY_SECRET'),
+          },
+          actionOptions: {
+            upload: {},
+            uploadStream: {},
+            delete: {},
+          },
+        },
+      },
+
+
+
+});
